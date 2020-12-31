803259284df483978dcb1e68ec8adb4d019d7f45
Alex Younger <alexanderyounger@gmail.com>
2020-12-31 16:16:26 -0500

    cleaned code

--- script.js
+++ script.js
@@ -1,90 +1,99 @@
 const gridContainer = document.querySelector("#grid-container");
+const buttons = document.querySelectorAll(".colorselector");
+const shake = document.querySelector("button");
+const gridSize = document.querySelector("#gridsize");
+const colorMode = document.querySelector("#colormode");
+const modal = document.querySelector("#myModal");
 
 const populate = (x) => {
-    if (x > 200) {
-        gridContainer.style.gridTemplate = `repeat(16, 1fr) / repeat(16, 1fr)`;
-        alert("That would be way too much work. Try a lower number.");
-        return;
-    }
-    gridContainer.style.gridTemplate = `repeat(${x}, 1fr) / repeat(${x}, 1fr)`;
-    let totalBlocks = x ** 2;
-    for (let i = 0; i < totalBlocks; i++) {
-        let gridBlock = document.createElement("div");
-        gridBlock.classList.add("grid-block", "blackandwhite");
-        gridContainer.appendChild(gridBlock);
-    }
+  if (x > 200) {
+    gridContainer.style.gridTemplate = `repeat(16, 1fr) / repeat(16, 1fr)`;
+    alert("That would be way too much work. Try a lower number.");
+    return;
+  }
+  gridContainer.style.gridTemplate = `repeat(${x}, 1fr) / repeat(${x}, 1fr)`;
+  let totalBlocks = x ** 2;
+  for (let i = 0; i < totalBlocks; i++) {
+    const gridBlock = document.createElement("div");
+    gridBlock.classList.add("grid-block", "blackandwhite");
+    gridContainer.appendChild(gridBlock);
+  }
+};
+
+const randomColor = () =>
+  `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+
+const shader = (item) => {
+  if (item.style.backgroundColor == "") {
+    item.style.backgroundColor = "rgb(255, 255, 255)";
+  }
+  let getColor = item.style.backgroundColor;
+  getColor = getColor.slice(4, getColor.length - 1).split(",");
+  getColor = getColor.map((i) => Number(i));
+  if (getColor[0] >= 25) {
+    getColor = getColor.map((j) => (j -= 25));
+  } else if (getColor[0] < 25) {
+    getColor.map((i) => (i = 0));
+  }
+  item.style.backgroundColor = `rgb(${getColor[0]}, ${getColor[1]}, ${getColor[2]})`;
+};
+
+const handleShade = (e) => {
+  if (e.target.classList.contains("blackandwhite")) {
+    e.target.style.backgroundColor = "black";
+  } else if (e.target.classList.contains("randomcolor")) {
+    e.target.style.backgroundColor = randomColor();
+  } else if (e.target.classList.contains("shader")) {
+    shader(e.target);
+  }
+};
+
+const shakeFunction = () => {
+  gridContainer.childNodes.forEach(
+    (block) => (block.style.backgroundColor = "rgb(255, 255, 255)")
+  );
+};
+
+const closeModal = () => {
+  modal.style.display = "none"
 }
 
-let randomColor = () => `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+gridContainer.addEventListener("mouseover", handleShade);
+gridContainer.addEventListener("touchstart", handleShade);
+shake.addEventListener("click", shakeFunction);
 
-let shader = (item) => {
-    if (item.style.backgroundColor == ""){
-        item.style.backgroundColor = "rgb(255, 255, 255)";
-    }
-    let getColor = item.style.backgroundColor;
-    getColor = getColor.slice(4, getColor.length - 1).split(",");
-    getColor = getColor.map(i => Number(i));
-    if (getColor[0] >= 25){
-        getColor = getColor.map(j => j -= 25);
-    } else if (getColor[0] < 25) {
-        getColor.map(i => i = 0);
-    }
-    item.style.backgroundColor = `rgb(${getColor[0]}, ${getColor[1]}, ${getColor[2]})`;
-}
-
-gridContainer.addEventListener("mouseover", e => {
-    if (e.target.classList.contains("blackandwhite")){
-        e.target.style.backgroundColor = "black";
-    } else if (e.target.classList.contains("randomcolor")){
-        e.target.style.backgroundColor = randomColor();
-    } else if (e.target.classList.contains("shader")){
-        shader(e.target);
-    }
+gridSize.addEventListener("click", () => {
+  let i = prompt("Please enter the row/column size as a number.");
+  if (i === null || i === "") {
+    return;
+  } else if (typeof i === "number" && i > 0) {
+    shakeFunction();
+    populate(i);
+  } else {
+    alert(
+      "I thought the instructions were rather simple. I guess I thought wrong."
+    );
+  }
 });
 
-gridContainer.addEventListener("touchstart", e => {
-    if (e.target.classList.contains("blackandwhite")){
-        e.target.style.backgroundColor = "black";
-    } else if (e.target.classList.contains("randomcolor")){
-        e.target.style.backgroundColor = randomColor();
-    } else if (e.target.classList.contains("shader")){
-        shader(e.target);
+colorMode.addEventListener("click", () => {
+  const span = document.querySelector(".close");
+  modal.style.display = "flex";
+  span.addEventListener("click", closeModal);
+  modal.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) {
+      closeModal()
     }
+  });
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    gridContainer.childNodes.forEach((node) => (node.className = e.target.id));
+    shakeFunction();
+  });
 });
 
 populate(16);
-
-const shakeFunction = () => Array.from(gridContainer.children).forEach(block => block.style.backgroundColor = "rgb(255, 255, 255)");
-
-const shake = document.querySelector("button");
-shake.addEventListener("click", () => shakeFunction());
-
-let gridSize = document.querySelector("#gridsize");
-gridSize.addEventListener("click", () => {
-    let i = prompt("Please enter the row/column size as a number.");
-    if (i == null || i == "") {
-        return;
-    }
-    else if (typeof i == "number" || i > 0) {
-        shakeFunction();
-        populate(i);
-    } else {alert("I thought the instructions were rather simple. I guess I thought wrong.")
-    }
-})
-
-let colorMode = document.querySelector("#colormode");
-let modal = document.querySelector("#myModal");
-colorMode.addEventListener("click", () => {
-    let span = document.querySelector(".close");
-    modal.style.display = "block";
-    span.addEventListener("click", () => modal.style.display = "none");
-    window.onclick = e => {if (e.target == modal) {
-        modal.style.display = "none";
-    }}
-})
-
-let buttons = Array.from(document.querySelectorAll(".colorselector"));
-buttons.forEach(button => button.addEventListener("click", e => {
-    Array.from(gridContainer.children).forEach(i => i.className = e.target.id);
-    shakeFunction();
-}));
